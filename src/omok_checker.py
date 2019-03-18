145eea62a5f8b88f9b35c9fbd6e60d89b5e58c81
StuartSul <ssstuartss@gmail.com>
2019-03-18 19:44:38 +0900

    changed method name __checkdefeat_r to count

--- src/omok_checker.py
+++ src/omok_checker.py
@@ -2,31 +2,31 @@ class Omok_checker:
     """Checks for conclusion of game or violation of rules"""
     @staticmethod
     def checkdefeat(board, i, j):
-        a = Omok_checker.__checkdefeat_r(board, i, j, -1, -1, board[i][j])
-        b = Omok_checker.__checkdefeat_r(board, i, j, 1, 1, board[i][j])
+        a = Omok_checker.count(board, i, j, -1, -1, board[i][j])
+        b = Omok_checker.count(board, i, j, 1, 1, board[i][j])
         if (a + b - 1) == 5:
             return True
-        a = Omok_checker.__checkdefeat_r(board, i, j, -1, 0, board[i][j])
-        b = Omok_checker.__checkdefeat_r(board, i, j, 1, 0, board[i][j])
+        a = Omok_checker.count(board, i, j, -1, 0, board[i][j])
+        b = Omok_checker.count(board, i, j, 1, 0, board[i][j])
         if (a + b - 1) == 5:
             return True
-        a = Omok_checker.__checkdefeat_r(board, i, j, -1, 1, board[i][j])
-        b = Omok_checker.__checkdefeat_r(board, i, j, 1, -1, board[i][j])
+        a = Omok_checker.count(board, i, j, -1, 1, board[i][j])
+        b = Omok_checker.count(board, i, j, 1, -1, board[i][j])
         if (a + b - 1) == 5:
             return True
-        a = Omok_checker.__checkdefeat_r(board, i, j, 0, 1, board[i][j])
-        b = Omok_checker.__checkdefeat_r(board, i, j, 0, -1, board[i][j])
+        a = Omok_checker.count(board, i, j, 0, 1, board[i][j])
+        b = Omok_checker.count(board, i, j, 0, -1, board[i][j])
         if (a + b - 1) == 5:
             return True
 
         return False
 
     @staticmethod
-    def __checkdefeat_r(board, i, j, idir, jdir, color):
+    def count(board, i, j, idir, jdir, color):
         if i < 0 or j < 0 or i >= len(board) or j >= len(board[0]):
             return 0
         if (board[i][j] == color):
-            return 1 + Omok_checker.__checkdefeat_r(board, i + idir, j + jdir, idir, jdir, color)
+            return 1 + Omok_checker.count(board, i + idir, j + jdir, idir, jdir, color)
         else:
             return 0
 
@@ -45,7 +45,7 @@ class Omok_checker:
 
     @staticmethod
     def isempty(board):
-        for i in for i in range (len(board)):
+        for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] != -1:
                     return False
