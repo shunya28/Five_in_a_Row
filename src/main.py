076e01c29626f1f2c157e61975836bbf8ff294c9
StuartSul <ssstuartss@gmail.com>
2019-03-27 21:27:12 +0900

    minor change

--- src/main.py
+++ src/main.py
@@ -7,5 +7,6 @@ boardheight = 25
 boardwidth = 25
 
 board = Omok_board(boardheight, boardwidth)
-ai = Omok_ai(board, 1)
+#ai0 = Omok_ai(board, 0)
+ai1 = Omok_ai(board, 1)
 gui = Omok_gui(windowtitle, board)
