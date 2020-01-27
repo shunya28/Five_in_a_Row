1521a6ab8a18597495368e9f29cbb8b0dd3cefaa
StuartSul <ssstuartss@gmail.com>
2020-01-28 01:15:27 +0900

    fixed AI framework to add nerual network based algorithm

--- omok/ai/ai.py
+++ omok/ai/ai.py
@@ -1,6 +1,7 @@
 from threading import Thread
 from time import sleep
 from omok.ai.minmax import MinMax
+from omok.ai.network import Network
 from omok.core.board import Board
 
 class AI:
@@ -11,15 +12,17 @@ class AI:
         self.exit_flag = False
         self.board.print('Omok AI initiated')
 
-    def load(self, status_condition):
+    def load(self, status_condition, ai_type):
         if len(self.threads) >= 2:
             self.board.print('No more AI threads can be created')
         elif status_condition != Board.BLACK_TURN and status_condition != Board.WHITE_TURN:
             self.board.print('Invalid status condition for AI')
         elif len(self.threads) == 1 and status_condition == self.threads[0][1]:
             self.board.print('Cannot create duplicate AI threads with the same status condition')
+        elif ai_type != 'minmax' and ai_type != 'network':
+            self.board.print('Invalid AI type; must be either "minmax" or "network"')
         else:
-            self.threads.append((Thread(target=lambda : self.play(status_condition)), status_condition))
+            self.threads.append((Thread(target=lambda : self.play(status_condition, ai_type)), status_condition))
             self.board.print('Omok AI loaded with condition ' + str(status_condition))
     
     def start(self):
@@ -34,12 +37,13 @@ class AI:
             thread[0].join()
         self.board.print('Omok AI stopped')
 
-    def play(self, status_condition):
-        sleep(2.0)
+    def play(self, status_condition, ai_type):
+        algorithm = MinMax() if ai_type == 'minmax' else Network()
+        sleep(2.0) # To prevent it from starting before GUI loads up
         while not self.exit_flag:
             if self.board.status == status_condition:
                 self.board.lock.acquire()
-                (i, j) = MinMax.decide_next_move(self.board.board, self.board.empty_slots, status_condition)
+                (i, j) = algorithm.decide_next_move(self.board, status_condition)
                 self.board.lock.release()
                 self.board.print('AI - ', end='')
                 self.board.place(i, j)
